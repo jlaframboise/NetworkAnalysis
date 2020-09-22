d21cda1ae561b214bd4af1f05642b414d97f1a3c
jlaframboise <jlaframboise02@gmail.com>
2020-09-22 11:54:43 -0400

    Used Geoip2 to retrieve location from ip.

---
+++ ip-to-location.py
@@ -0,0 +1,17 @@
+import geoip2.database
+
+geoDataPath = '/home/jacob/Projects/NetworkAnalysis/GeoLite2-City_20200915/GeoLite2-City.mmdb'
+
+with geoip2.database.Reader(geoDataPath) as reader:
+    
+    response=reader.city('194.147.201.244')
+    print(response.country.iso_code)
+    print(response.country.name)
+    print(response.subdivisions.most_specific.name)
+    print(response.subdivisions.most_specific.iso_code)
+    print(response.city.name)
+    print(response.postal.code)
+    print(response.location.latitude)
+    print(response.location.longitude)
+    print(response.traits.network)
+
