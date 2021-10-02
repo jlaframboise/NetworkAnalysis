1da673cbf376c231588d8ae0a414ecbecbdf4ace
jlaframboise <jlaframboise02@gmail.com>
2021-10-02 11:46:58 -0400

    Set up the MUI React Dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { ReactDOM } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import Button from '@mui/material/Button';
+
+import Dashboard from './components/Dashboard.js';
 
 function App() {
 
@@ -13,23 +18,26 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}</p>
-      </header>
-    </div>
+    Dashboard()
+    // <div className="App">
+    //   <header className="App-header">
+        
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <p>The current time is {currentTime}</p>
+    //     <Button variant="contained">Hello world from Material UI!</Button>
+    //   </header>
+    // </div>
   );
 }
 
